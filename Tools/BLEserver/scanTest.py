d1064f2d3f0c90cc5dee6f47dc75466068b918b8
PedroArtDesk <pedroarteta@hotmail.es>
2025-10-02 03:00:45 -0500

    lector ble

---
+++ Tools/BLEserver/scanTest.py
@@ -0,0 +1,11 @@
+# pip install bleak
+import asyncio
+from bleak import BleakScanner
+
+async def main():
+    devices = await BleakScanner.discover(timeout=5.0)
+    for d in devices:
+        print(d)
+
+if __name__ == "__main__":
+    asyncio.run(main())
